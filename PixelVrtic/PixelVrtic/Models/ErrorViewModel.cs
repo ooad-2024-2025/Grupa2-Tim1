75bcdec89aa785b50ef49e463bf54a7a00b61869
nottarik <tfetahovic1@etf.unsa.ba>
2025-04-15 10:57:35 +0200

    c# aplikacija template

---
+++ PixelVrtic/PixelVrtic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PixelVrtic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
