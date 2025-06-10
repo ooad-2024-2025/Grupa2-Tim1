f59630801baa70cba72935d9ad1a660e3407498c
nottarik <tfetahovic1@etf.unsa.ba>
2025-06-10 11:38:53 +0200

    lile

---
+++ PixelVrtic/PixelVrtic/Views/Roditelj/Finansije.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PixelVrtic.Views.Roditelj
+{
+    public class FinansijeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
